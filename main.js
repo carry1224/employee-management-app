f47f29aab1eb29d986686028e85e5efbfb24125f
carry1224 <carrycathrine.gmail.com>
2026-03-30 10:28:27 +0300

    added greeting according to time of day

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ links.forEach(link => {
 });
 const heading = document.getElementById('heading');
 const para = document.getElementById('para');
+
+const hour = new Date().getHours();
+let greeting = '';
+
+if (hour < 12) greeting = 'Good Morning! 🌅';
+else if (hour < 18) greeting = 'Good Afternoon! ☀️';
+else greeting = 'Good Evening! 🌙';
+
+if (heading && !heading.innerHTML.includes(greeting)) {
+heading.innerHTML = `${greeting}<br>${heading.innerHTML}`;
+}
