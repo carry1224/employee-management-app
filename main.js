cff965d1610e9ff6f9c4e59468524fad20cdecde
carry1224 <carrycathrine.gmail.com>
2026-03-30 10:24:31 +0300

    set active links based on current pages

---
+++ main.js
@@ -0,0 +1,9 @@
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+const links = document.querySelectorAll('.nav-links li a');
+
+links.forEach(link => {
+    const linkPage = link.getAttribute('href');
+    if (linkPage === currentPage) {
+        link.classList.add('active');
+    }
+});
